b191e4ac91302db7b5701744c1b2de0a86902dda
Alec M <alecmattu1234@gmail.com>
2023-05-29 08:53:04 -0400

    Stylize SearchContainer
    Signed-off-by: Alec M <alecmattu1234@gmail.com>

--- src/components/SearchContainer.tsx
+++ src/components/SearchContainer.tsx
@@ -1,16 +1,20 @@
 import React, { FC } from "react";
 
-type Props = {
+type Props = {};
 
-};
-
-const SearchContainer : FC<Props> = () => {
+const SearchContainer: FC<Props> = () => {
   return (
-    <div>
-      Find it. Tour it. Own it.
+    <div className="search-banner pt-12 text-center h-96">
+      <div className="text-4xl font-medium text-white my-5">
+        Find it. Tour it. Own it.
+      </div>
 
       <div>
-        <input type="text" placeholder="Enter an address, neighborhood, city, or ZIP code" />
+        <input
+          className="p-4 py-5 w-2/6 rounded shadow-xl focus:outline-none focus:ring-4 placeholder-gray-500 font-medium"
+          type="text"
+          placeholder="Enter an address, neighborhood, city, or ZIP code"
+        />
       </div>
     </div>
   );
