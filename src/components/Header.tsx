b323b691c72343f8fa57de044f8697353356b783
Alec M <alecmattu1234@gmail.com>
2023-06-03 16:06:29 -0400

    Fix header styling
    Signed-off-by: Alec M <alecmattu1234@gmail.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,42 +6,44 @@ type Props = {
 
 const Header : FC<Props> = () => {
   return (
-    <div className="flex items-center justify-center py-7 w-full bg-white">
+    <div className="flex items-center justify-center py-6 w-full bg-white text-gray-700 cursor-pointer">
       {/* Section One */}
-      <div className="grid grid-cols-5 gap-3 ml-auto">
-        <div>
+      <div className="flex items-center justify-center gap-x-5 ml-auto">
+        <div className="hover:text-blue-600">
           Buy
         </div>
-        <div>
+        <div className="hover:text-blue-600">
           Rent
         </div>
-        <div>
+        <div className="hover:text-blue-600">
           Sell
         </div>
-        <div>
+        <div className="hover:text-blue-600">
           Home Loans
         </div>
-        <div>
+        <div className="hover:text-blue-600">
           Agent Finder
         </div>
       </div>
 
       {/* Icon */}
       <div className="flex items-center justify-center mx-auto">
-        <img src="https://s.zillowstatic.com/pfs/static/z-logo-default.svg" alt="logo" height={25} width={120} />
+        <img src="https://s.zillowstatic.com/pfs/static/z-logo-default.svg" alt="logo" height="auto" width={160} />
       </div>
 
       {/* Section Two */}
-      <div className="grid grid-cols-5 gap-3 mr-auto">
-        <div>
+      <div className="flex items-center justify-center gap-x-5 mr-auto">
+        <div className="hover:text-blue-600">
           Manage Rentals
         </div>
-        <div>
+        <div className="hover:text-blue-600">
           Advertise
         </div>
-        <div>
+        <div className="hover:text-blue-600">
           Help
         </div>
+        <div></div>
+        <div></div>
       </div>
     </div>
   );
