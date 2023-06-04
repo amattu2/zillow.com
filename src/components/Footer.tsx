04c26e6a990b9950eb670404d2263836185faa5a
Alec M <alecmattu1234@gmail.com>
2023-06-04 10:19:54 -0400

    Add styled footer
    Signed-off-by: Alec M <alecmattu1234@gmail.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,9 +6,85 @@ type Props = {
 
 const Footer : FC<Props> = () => {
   return (
-    <>
-      <div>Footer</div>
-    </>
+    <div className="container py-10 mx-auto">
+      <div className="grid grid-flow-col auto-cols-min gap-x-8 text-center justify-center border-b border-b-gray-200 pb-10">
+        <div className="flex justify-center items-center border-e border-e-black w-72">
+          <h1 className="text-lg font-normal hover:underline cursor-pointer">Real Estate</h1>
+          <p className="ml-5">
+            <svg className="fill-blue-600" height="16px" width="16px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 330 330">
+              <path id="XMLID_225_" d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393
+                c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393
+                s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"/>
+            </svg>
+          </p>
+        </div>
+        <div className="flex justify-center items-center border-e border-e-black w-72">
+          <h1 className="text-lg font-normal hover:underline cursor-pointer">Rentals</h1>
+          <p className="ml-5">
+            <svg className="fill-blue-600" height="16px" width="16px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 330 330">
+              <path id="XMLID_225_" d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393
+                c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393
+                s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"/>
+            </svg>
+          </p>
+        </div>
+        <div className="flex justify-center items-center border-e border-e-black w-72">
+          <h1 className="text-lg font-normal hover:underline cursor-pointer">Mortgage Rates</h1>
+          <p className="ml-5">
+            <svg className="fill-blue-600" height="16px" width="16px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 330 330">
+              <path id="XMLID_225_" d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393
+                c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393
+                s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"/>
+            </svg>
+          </p>
+        </div>
+        <div className="flex justify-center items-center w-72">
+          <h1 className="text-lg font-normal hover:underline cursor-pointer">Browse Homes</h1>
+          <p className="ml-5">
+            <svg className="fill-blue-600" height="16px" width="16px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 330 330">
+              <path id="XMLID_225_" d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393
+                c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393
+                s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"/>
+            </svg>
+          </p>
+        </div>
+      </div>
+      <div className="grid grid-cols-10 gap-6 text-center justify-center border-b border-b-gray-200 py-8 text-gray-700 text-sm">
+        <a href="/" className="hover:underline cursor-pointer">About</a>
+        <a href="/" className="hover:underline cursor-pointer">Zestimates</a>
+        <a href="/" className="hover:underline cursor-pointer">Research</a>
+        <a href="/" className="hover:underline cursor-pointer">Careers</a>
+        <a href="/" className="hover:underline cursor-pointer">Help</a>
+        <a href="/" className="hover:underline cursor-pointer">Advertise</a>
+        <a href="/" className="hover:underline cursor-pointer">Fair Housing Guide</a>
+        <a href="/" className="hover:underline cursor-pointer">Terms of use</a>
+        <a href="/" className="hover:underline cursor-pointer">Privacy Portal</a>
+        <a href="/" className="hover:underline cursor-pointer">Cookie Preference</a>
+        <a href="/" className="hover:underline cursor-pointer">Blog</a>
+        <a href="/" className="hover:underline cursor-pointer">AI</a>
+        <a href="/" className="hover:underline cursor-pointer">Mobile Apps</a>
+        <a href="/" className="hover:underline cursor-pointer">Truila</a>
+        <a href="/" className="hover:underline cursor-pointer">StreetEasy</a>
+        <a href="/" className="hover:underline cursor-pointer">HotPads</a>
+        <a href="/" className="hover:underline cursor-pointer">Out East</a>
+        <a href="/" className="hover:underline cursor-pointer">ShowingTime+</a>
+      </div>
+      <div className="text-center text-gray-700 text-xs py-10 max-w-2xl mx-auto">
+        <p className="mb-8">
+          Zillow Group is committed to ensuring digital accessibility for individuals with disabilities. We are continuously working to improve the accessibility of our web experience for everyone, and we welcome feedback and accommodation requests. If you wish to report an issue or seek an accommodation, please let us know.
+        </p>
+        <p className="mb-8">
+          Zillow, Inc. holds real estate brokerage licenses in multiple states. Zillow (Canada), Inc. holds real estate brokerage licenses in multiple provinces.
+          <br/>§ 442-H New York Standard Operating Procedures
+          <br/>§ New York Fair Housing Notice
+          <br/>TREC: Information about brokerage services, Consumer protection notice
+          <br/>California DRE #1522444
+        </p>
+        <p>
+          For listings in Canada, the trademarks REALTOR®, REALTORS®, and the REALTOR® logo are controlled by The Canadian Real Estate Association (CREA) and identify real estate professionals who are members of CREA. The trademarks MLS®, Multiple Listing Service® and the associated logos are owned by CREA and identify the quality of services provided by real estate professionals who are members of CREA. Used under license.
+        </p>
+      </div>
+    </div>
   );
 };
 
